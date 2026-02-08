16dc8303ac6bac7d4a3cee4a269dbe955e781db1
Ahmad Iqbal <iqbalgamez18@gmail.com>
2026-02-08 19:05:34 +0700

    script.js

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
-/* LOADER */
-window.onload = () => {
-  setTimeout(() => {
-    document.getElementById("loader").style.display = "none"
-  }, 800)
-}
-
-/* NAV AUTO HIDE */
-let last = 0
-const nav = document.getElementById("nav")
-
-window.addEventListener("scroll", () => {
-  let y = window.scrollY
-  nav.classList.toggle("hide", y > last && y > 80)
-  last = y
-})
-
-/* SCROLL ANIMATION */
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show")
-    }
-  })
-})
-
-document.querySelectorAll(".card").forEach(el => observer.observe(el))
+function wa() {
+  let nomor = "6281271617785"; // GANTI NOMOR WA KAMU
+  let pesan = "Halo, saya mau bertanya/bikin jasa pembuatan website";
+  window.open(`https://wa.me/${nomor}?text=${encodeURIComponent(pesan)}`, "_blank");
