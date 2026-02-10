21c91ca3bf8d547f02ec037e2e6ec7f14c96e613
Ahmad Iqbal <iqbalgamez18@gmail.com>
2026-02-10 18:34:55 +0700

    script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-function wa() {
-  let nomor = "6281271617785"; // GANTI NOMOR WA KAMU
-  let pesan = "Halo, saya mau bertanya/bikin jasa pembuatan website";
-  window.open(`https://wa.me/${nomor}?text=${encodeURIComponent(pesan)}`, "_blank");
